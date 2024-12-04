6c0eba7381e7b1bcfc8d9a1724f5a6f63570d8c9
IvonaPuncheva <ivpuncheva@gmail.com>
2024-12-04 11:48:56 +0200

    Update register.component.ts

--- blog-app/src/app/pages/register/register.component.ts
+++ blog-app/src/app/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +15,7 @@ export class RegisterComponent {
   registerForm: FormGroup;
   message: string = '';
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
     this.registerForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
@@ -22,14 +23,34 @@ export class RegisterComponent {
     });
   }
 
+  // onRegister() {
+  //   if (this.registerForm.valid) {
+  //     this.http.post('http://localhost:5000/register', this.registerForm.value)
+  //       .subscribe(
+  //         (res: any) => {
+  //           this.message = res.message || 'Registration successful!';
+  //         },
+  //         (err) => {
+  //           this.message = err.error.message || 'An error occurred during registration.';
+  //           this.router.navigate(['/login']);
+  //         }
+  //       );
+  //   } else {
+  //     this.message = 'Please fill in all fields correctly.';
+  //   }
+  // }
   onRegister() {
     if (this.registerForm.valid) {
       this.http.post('http://localhost:5000/register', this.registerForm.value)
         .subscribe(
           (res: any) => {
+            // Показване на съобщение за успешна регистрация
             this.message = res.message || 'Registration successful!';
+            // Пренасочване към login
+            this.router.navigate(['/login']);
           },
           (err) => {
+            // Показване на грешка, ако има проблем при регистрацията
             this.message = err.error.message || 'An error occurred during registration.';
           }
         );
@@ -37,4 +58,5 @@ export class RegisterComponent {
       this.message = 'Please fill in all fields correctly.';
     }
   }
+  
 }
