64c85db4cf76d88c9c54b8e6d74b943d6af877bf
IvonaPuncheva <ivpuncheva@gmail.com>
2024-12-08 00:44:38 +0200

    Added the "latest add" functionality to the home page

--- blog-app/src/app/pages/home/home.component.ts
+++ blog-app/src/app/pages/home/home.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  latestAds: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadLatestAds();
+  }
+
+  loadLatestAds() {
+    this.http.get<any[]>('http://localhost:5000/ads').subscribe(
+      (ads) => {
+
+        this.latestAds = ads.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()).slice(0, 3);
+      },
+      (error) => {
+        console.error('Error fetching latest ads:', error);
+      }
+    );
+  }
 }
