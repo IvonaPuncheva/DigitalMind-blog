817fb532dabd1440b04bf5ce50b1a6af612c4807
IvonaPuncheva <ivpuncheva@gmail.com>
2024-12-11 22:33:11 +0200

    update home css

--- blog-app/src/app/pages/catalog/catalog.component.ts
+++ blog-app/src/app/pages/catalog/catalog.component.ts
@@ -1,7 +1,82 @@
 
+// import { Component, OnInit, Inject } from '@angular/core';
+// import { AdsService } from '../../services/ads.service';
+// import { CommonModule, isPlatformBrowser } from '@angular/common';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Router } from '@angular/router';
+// import { PLATFORM_ID } from '@angular/core';
+// import { MatSnackBar } from '@angular/material/snack-bar';
+
+// @Component({
+//   selector: 'app-catalog',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './catalog.component.html',
+//   styleUrls: ['./catalog.component.css']
+// })
+// export class CatalogComponent implements OnInit {
+//   ads: any[] = []; 
+
+//   constructor(
+//     private adsService: AdsService,
+//     private http: HttpClient,
+//     private router: Router,
+//     private _snackBar: MatSnackBar,
+//     @Inject(PLATFORM_ID) private platformId: Object 
+//   ) {}
+
+//   loadAds() {
+//     this.http.get<any[]>('http://localhost:5000/ads').subscribe(
+//       (res) => {
+//         this.ads = res; 
+//       },
+//       (err) => {
+//         console.error('Error loading ads:', err);
+//       }
+//     );
+//   }
+
+ 
+// likeAd(adId: string) {
+//   const token = localStorage.getItem('authToken');
+//   if (!token) {
+//     this._snackBar.open('You need to be logged in to like an ad.', 'Close', { duration: 3000 });
+//     return;
+//   }
+
+//   this.http.post(`http://localhost:5000/ads/${adId}/like`, {}, {
+//     headers: { Authorization: `Bearer ${token}` }
+//   }).subscribe(
+//     (res: any) => {
+//       const ad = this.ads.find(ad => ad._id === adId);
+//       if (ad) {
+//         ad.likes = res.likes;
+//         this._snackBar.open('Ad liked successfully!', 'Close', { duration: 3000 });
+//       }
+//     },
+//     (err) => {
+//       if (err.status === 400 && err.error.message === 'You have already liked this ad.') {
+//         this._snackBar.open('You have already liked this ad.', 'Close', { duration: 3000 });
+//       } else {
+//         console.error('Error liking ad:', err);
+//       }
+//     }
+//   );
+// }
+
+// navigateToDetails(adId: string) {
+//   this.router.navigate(['/details', adId]);
+// }
+
+//   ngOnInit() {
+//     this.loadAds(); 
+//   }
+// }
+
+
 import { Component, OnInit, Inject } from '@angular/core';
 import { AdsService } from '../../services/ads.service';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { PLATFORM_ID } from '@angular/core';
@@ -16,6 +91,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class CatalogComponent implements OnInit {
   ads: any[] = []; 
+  displayedAds: any[] = []; 
+  currentPage: number = 1; 
+  totalPages: number = 1; 
+  pageSize: number = 5; 
 
   constructor(
     private adsService: AdsService,
@@ -29,6 +108,8 @@ export class CatalogComponent implements OnInit {
     this.http.get<any[]>('http://localhost:5000/ads').subscribe(
       (res) => {
         this.ads = res; 
+        this.totalPages = Math.ceil(this.ads.length / this.pageSize); 
+        this.updateDisplayedAds(); 
       },
       (err) => {
         console.error('Error loading ads:', err);
@@ -36,41 +117,55 @@ export class CatalogComponent implements OnInit {
     );
   }
 
- 
-likeAd(adId: string) {
-  const token = localStorage.getItem('authToken');
-  if (!token) {
-    this._snackBar.open('You need to be logged in to like an ad.', 'Close', { duration: 3000 });
-    return;
+
+  updateDisplayedAds() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.displayedAds = this.ads.slice(startIndex, endIndex); 
   }
 
-  this.http.post(`http://localhost:5000/ads/${adId}/like`, {}, {
-    headers: { Authorization: `Bearer ${token}` }
-  }).subscribe(
-    (res: any) => {
-      const ad = this.ads.find(ad => ad._id === adId);
-      if (ad) {
-        ad.likes = res.likes;
-        this._snackBar.open('Ad liked successfully!', 'Close', { duration: 3000 });
-      }
-    },
-    (err) => {
-      if (err.status === 400 && err.error.message === 'You have already liked this ad.') {
-        this._snackBar.open('You have already liked this ad.', 'Close', { duration: 3000 });
-      } else {
-        console.error('Error liking ad:', err);
-      }
-    }
-  );
-}
 
-navigateToDetails(adId: string) {
-  this.router.navigate(['/details', adId]);
-}
+  changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.updateDisplayedAds(); 
+    }
+  }
+
+
+  likeAd(adId: string) {
+    const token = localStorage.getItem('authToken');
+    if (!token) {
+      this._snackBar.open('You need to be logged in to like an ad.', 'Close', { duration: 3000 });
+      return;
+    }
+
+    this.http.post(`http://localhost:5000/ads/${adId}/like`, {}, {
+      headers: { Authorization: `Bearer ${token}` }
+    }).subscribe(
+      (res: any) => {
+        const ad = this.ads.find(ad => ad._id === adId);
+        if (ad) {
+          ad.likes = res.likes;
+          this._snackBar.open('Ad liked successfully!', 'Close', { duration: 3000 });
+        }
+      },
+      (err) => {
+        if (err.status === 400 && err.error.message === 'You have already liked this ad.') {
+          this._snackBar.open('You have already liked this ad.', 'Close', { duration: 3000 });
+        } else {
+          console.error('Error liking ad:', err);
+        }
+      }
+    );
+  }
+
+
+  navigateToDetails(adId: string) {
+    this.router.navigate(['/details', adId]);
+  }
 
   ngOnInit() {
     this.loadAds(); 
   }
 }
-
-
