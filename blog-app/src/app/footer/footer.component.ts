71278874b81dcef08d1f269e41000be1a58bed7d
IvonaPuncheva <ivpuncheva@gmail.com>
2024-12-11 20:40:30 +0200

    added general styles

--- blog-app/src/app/footer/footer.component.ts
+++ blog-app/src/app/footer/footer.component.ts
@@ -8,5 +8,5 @@ import { Component } from "@angular/core";
 })
 
 export class FooterComponent{
-    title ="Ivona"
+    title ="Ivona Puncheva"
 }
--- blog-app/src/app/navigation-bar/nav-bar.component.ts
+++ blog-app/src/app/navigation-bar/nav-bar.component.ts
@@ -8,49 +8,26 @@ import { AuthenticationService } from "../services/authentication.service";
   selector: 'app-nav-bar',
   standalone: true,
   template: `
-    <div class="nav-bar">
-      <div>{{ title }}</div>
-      <a class="nav-btn" href="/home">Home</a>
-      <a class="nav-btn" href="/about">About</a>
-      <a class="nav-btn" href="/catalog">Catalog</a>
-         <a *ngIf="authService.isLoggedIn()" class="nav-btn" href="/create">Create</a>
-
-
-      <a *ngIf="!authService.isLoggedIn()" class="nav-btn" href="/login">Login</a>
-      <a *ngIf="!authService.isLoggedIn()" class="nav-btn" href="/register">Register</a>
-
-    
-      <a *ngIf="authService.isLoggedIn()" (click)="logout()" class="nav-btn">Logout</a>
-    </div>
+   <nav class="navbar">
+      <div class="navbar-brand">{{ title }}</div>
+      <div class="navbar-center">
+        <a class="nav-link" href="/home">Home</a>
+        <a class="nav-link" href="/about">About</a>
+        <a class="nav-link" href="/catalog">Catalog</a>
+        <a *ngIf="authService.isLoggedIn()" class="nav-link" href="/create">Create</a>
+      </div>
+      <div class="navbar-right">
+        <a *ngIf="!authService.isLoggedIn()" class="nav-link" href="/login">Login</a>
+        <a *ngIf="!authService.isLoggedIn()" class="nav-link" href="/register">Register</a>
+        <a *ngIf="authService.isLoggedIn()" (click)="logout()" class="nav-link">Logout</a>
+      </div>
+    </nav>
   `,
-  styles: [`
-    .nav-bar {
-      width: 100%;
-      height: 80px;
-      padding: 20px;
-      background-color: skyblue;
-      color: #000;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    .nav-btn {
-      margin-left: 10px;
-      border: 1px solid black;
-      padding: 10px;
-      text-decoration: none;
-      color: black;
-    }
-    .nav-btn:hover {
-      cursor: pointer;
-      background-color: grey;
-      color: #fff;
-    }
-  `],
+
   imports: [CommonModule],  
 })
 export class NavBarComponent {
-  title = 'My Nav Component';
+  title = 'DIGITAL MIND';
 
   constructor(public authService: AuthenticationService) {}
 
