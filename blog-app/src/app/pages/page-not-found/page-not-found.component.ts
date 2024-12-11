f6f68b0f7cb1c8ba57c55d38175bef348dc124e0
IvonaPuncheva <ivpuncheva@gmail.com>
2024-12-11 14:34:35 +0200

    added css to home page

--- blog-app/src/app/pages/page-not-found/page-not-found.component.ts
+++ blog-app/src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,14 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-page-not-found',
-//   imports: [],
-//   templateUrl: './page-not-found.component.html',
-//   styleUrl: './page-not-found.component.css'
-// })
-// export class PageNotFoundComponent {
-
-// }
 
 import { Component } from '@angular/core';
 
